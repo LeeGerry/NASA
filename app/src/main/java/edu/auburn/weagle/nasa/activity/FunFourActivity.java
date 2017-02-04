134b95e672c630e6ec88d2a2d53ea3b52e7f9db0
guorui <proheart123@gmail.com>
2017-02-04 11:04:18 -0600

    update

--- app/src/main/java/edu/auburn/weagle/nasa/activity/FunFourActivity.java
+++ app/src/main/java/edu/auburn/weagle/nasa/activity/FunFourActivity.java
@@ -1,8 +1,12 @@
 package edu.auburn.weagle.nasa.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import edu.auburn.weagle.nasa.R;
+import edu.auburn.weagle.nasa.config.AppConfig;
 
 /**
  * Author: Gary
@@ -10,9 +14,35 @@ import edu.auburn.weagle.nasa.R;
  */
 
 public class FunFourActivity extends BaseActivity {
+//    private Button btnSelect;
+    private Spinner spinner;
+    private static int selected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_four);
+//        btnSelect = (Button) findViewById(btnSelect);
+        spinner = (Spinner) findViewById(R.id.spinner);
+//        btnSelect.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                chooseCamera();
+//            }
+//        });
+    }
+    private void chooseCamera(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(FunFourActivity.this);
+        builder.setTitle(getString(R.string.select_camera));
+        builder.setIcon(android.R.drawable.btn_star_big_on);
+        builder.setSingleChoiceItems(AppConfig.types, selected, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                selected = i;
+//                btnSelect.setText(AppConfig.types[i]);
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, null);
+        builder.show();
     }
 }
--- app/src/main/java/edu/auburn/weagle/nasa/config/AppConfig.java
+++ app/src/main/java/edu/auburn/weagle/nasa/config/AppConfig.java
@@ -6,6 +6,8 @@ package edu.auburn.weagle.nasa.config;
  */
 
 public class AppConfig {
+    public final static String[] types = {"FHAZ","RHAZ","MAST","CHEMCAM","MAHLI","MARDI","NAVCAM","PANCAM","MINITES"};
+
     public final static String SERVER = "https://api.nasa.gov";
     public final static String API_KEY = "&api_key=eVQWCl4aiAvDuNwvXzMFzvDQEZ2BakaANp03RVtI";
     public final static String sample = SERVER+"/mars-photos/api/v1/rovers/curiosity/photos?sol=1000"+API_KEY;
